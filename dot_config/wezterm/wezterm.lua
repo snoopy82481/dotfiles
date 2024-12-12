d3a16551c071d212347cca23355587060cc46bb8
Stephen Beale <stephen.r.beale@gmail.com>
2024-12-12 10:51:10 -0700

    refactor: Update for non personal machines

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,11 @@
+-- Pull in WezTerm API
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
+
+-- Initialize actual config
+local config = {}
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
 
 config.font_size = 16
 config.font = wezterm.font("MesloLGLDZ Nerd Font Mono")
@@ -12,9 +18,9 @@ config.max_fps = 120
 config.enable_kitty_graphics = true
 config.window_close_confirmation = "NeverPrompt"
 config.macos_window_background_blur = 12
-config.window_background_opacity = 0.67
+config.window_background_opacity = 1.0
 config.audible_bell = "Disabled"
-config.color_scheme = 'Gruvbox Material (Gogh)'
+config.color_scheme = "Gruvbox Material (Gogh)"
 
 config.window_padding = {
 	left = 18,
@@ -34,6 +40,11 @@ config.keys = {
 		key = "RightArrow",
 		mods = "OPT",
 		action = wezterm.action({ SendString = "\x1bf" }),
+	},
+	{
+		key = "A",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.QuickSelect,
 	},
 }
 
